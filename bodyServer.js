2dd02b0154e8be79ecd22743b7c4f18b5dfde3c3
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-09-28 11:11:29 +0530

    Accessing Body parameters from a request

---
+++ bodyServer.js
@@ -0,0 +1,36 @@
+const http=require('http');
+const { parse } = require('querystring');
+
+const hostName='localhost';
+const port = 3000;
+
+const server=http.createServer((req,res)=>{
+    const body = [];
+    const params=[];
+    if(req.method=="POST"){
+        
+
+        req.on("data", chunk => {
+            body.push(chunk);
+            let str=body.toString();
+            console.log();
+            params.push(str.slice(str.indexOf("data;")+5,str.length));
+        });
+        
+        
+    }
+    
+    
+    res.statusCode=200;
+    req.on("end", () => {
+        // converts buffer stream into readable string format
+        console.log(params);
+        let parsedBody =Buffer.concat(body).toString(); //
+        //console.log(parsedBody);
+        res.end(parsedBody);
+      });
+});
+
+server.listen(port,hostName,()=>{
+    console.log(`Server running at http://${hostName}:${port}`);
+});
