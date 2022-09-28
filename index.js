1d8f1fd2a57ad340f7271903655f5a010735e51e
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-09-28 12:46:44 +0530

    Setting up Express server Enviornment.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+
+const host = "localhost";
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<html>
+            <body>
+                <h1>
+                    Welcome To Harshvardhan's Express Server.
+                </h1>
+            </body>
+            </html>`
+        );
+});
+
+const server = http.createServer(app);
+server.listen(port, host, () => {
+  console.log(`Server Running at http://${host}:${port}`);
+});
