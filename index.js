4752fc0cf4809b1ed6f61891c8d455147c161c05
harshD-ksolves <harshvardhan.deshmukh@ksolves.com>
2022-09-28 13:25:18 +0530

    Basic routing

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const express = require("express");
-const http = require("http");
-
-const host = "localhost";
-const port = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log(req.headers);
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<html>
-            <body>
-                <h1>
-                    Welcome To Harshvardhan's Express Server.
-                </h1>
-            </body>
-            </html>`
-        );
+//Basic Routing
+app.get('/', (req,res)=>{
+    console.log(req.query);
+
+    res.status=200;
+    res.send(req.query?.name);
 });
 
-const server = http.createServer(app);
-server.listen(port, host, () => {
-  console.log(`Server Running at http://${host}:${port}`);
+app.post('/',(req,res)=>{
+    console.log(req.body);
+
+    res.status=200;
+    res.send(req.body);
+});
+
+app.put('/:id',(req,res)=>{
+    console.log(req.params?.id);
+    
+    res.status=200;
+    res.send([req.body,req.params]);
+});
+
+app.delete("/:id",(req,res)=>{
+    console.log(req.params?.id);
+
+    res.status=200;
+    res.send(req.params);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Express server is running at http://localhost:${process.env.PORT}`);
 });
